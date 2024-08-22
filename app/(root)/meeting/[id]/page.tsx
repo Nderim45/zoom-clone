080ab2106dc7e13c08d2a51a01d13014449e5ddd
unknown <nderim258@gmail.com>
2024-08-23 00:05:17 +0200

    Created Meeting Room page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -8,6 +8,7 @@ import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
+
 const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
   const { call, isCallLoading } = useGetCallById(id);
